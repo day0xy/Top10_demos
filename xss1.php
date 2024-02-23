b5145321f878302ed2c493b707d0b639774c8fe7
day0xy <day0xyz1@gmail.com>
2024-02-23 11:06:12 +0800

    2

--- xss1.php
+++ xss1.php
@@ -1,8 +1,8 @@
 <?php
 $name = htmlspecialchars($_GET['name']);//实体化name,包括<>'"
 $age = htmlspecialchars($_GET['age']);//实体化name,包括<>'"
-$str = '<script>var a="'.$name.'";var b="'.$age. '"</script>';
-if (strpos($name, "'")) {
+$str = '<script>var a="' . $name . '";var b="' . $age . '"</script>';
+if (strpos($name, "\"")) {
     echo 'waf';
 } else {
     echo $str;
