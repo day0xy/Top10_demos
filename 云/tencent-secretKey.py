91aada8a795dec760cb6b702b8712044bf0c4369
day0xy <day0xyz1@gmail.com>
2024-02-28 13:29:30 +0800

    add cloud

---
+++ 云/tencent-secretKey.py
@@ -0,0 +1,24 @@
+# -*- coding=utf-8
+from qcloud_cos import CosConfig
+from qcloud_cos import CosS3Client
+import sys
+import logging
+
+# https://www.tencentcloud.com/zh/document/product/1045/48105?lang=zh&pg=
+
+logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+secret_key = ''
+secret_id = ''
+region = ''
+token = ''
+scheme = 'https'
+
+config = CosConfig(Region=region, SecretId=secret_id,
+                   SecretKey=secret_key, Token=token, Scheme=scheme)
+client = CosS3Client(config)
+
+response = client.put_object(
+    Bucket='',
+    Key='a.txt',
+    Body='test'
+)
